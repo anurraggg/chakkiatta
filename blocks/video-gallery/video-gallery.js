004bf7d3ea2a18d47d6485ccb158eea72cbbab0f
anurraggg <anuragsengupta02@gmail.com>
2025-11-03 16:52:36 +0530

    cool

--- blocks/video-gallery/video-gallery.js
+++ blocks/video-gallery/video-gallery.js
@@ -79,7 +79,7 @@ export default function decorate(block) {
 
 // Helper: Extract YouTube ID from URL
 function extractYouTubeId(url) {
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#?]*).*/;
   const match = url.match(regExp);
   return (match && match[2].length === 11) ? match[2] : null;
 }
