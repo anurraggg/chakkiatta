7a230cbf1a1a2160b99ee4d733ca9871819539c3
anurraggg <anuragsengupta02@gmail.com>
2025-11-18 14:47:49 +0530

    adding analytics part 2

--- scripts/head.js
+++ scripts/head.js
@@ -1,28 +1,34 @@
 export default function decorate() {
-    /* ------------------------------
-       Load GA4 library
-    --------------------------------*/
-    const gaScript = document.createElement("script");
-    gaScript.async = true;
-    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-4DVHQ0LHTV";
-    document.head.appendChild(gaScript);
+
+    function initGA() {
+      // Load GA4 library
+      const gaScript = document.createElement("script");
+      gaScript.async = true;
+      gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-4DVHQ0LHTV";
+      document.head.appendChild(gaScript);
   
-    /* ------------------------------
-       Setup dataLayer + gtag()
-    --------------------------------*/
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){ dataLayer.push(arguments); }
-    window.gtag = gtag;
+      // Setup dataLayer + gtag when the script finishes loading
+      gaScript.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        window.gtag = gtag;
   
-    gtag('js', new Date());
+        gtag("js", new Date());
   
-    /* ----------------------------------
-       IMPORTANT FOR EDS:
-       Disable auto page_view tracking
-       because navigation is soft-SPA
-    -----------------------------------*/
-    gtag('config', 'G-4DVHQ0LHTV', {
-      send_page_view: false
-    });
+        // Important for EDS soft navigation
+        gtag("config", "G-4DVHQ0LHTV", {
+          send_page_view: false
+        });
+  
+        console.log("GA4 loaded successfully");
+      };
+    }
+  
+    // Make sure <head> exists before injecting the script
+    if (document.head) {
+      initGA();
+    } else {
+      document.addEventListener("DOMContentLoaded", initGA);
+    }
   }
   
